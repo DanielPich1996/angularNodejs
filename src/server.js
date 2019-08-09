a03db8a35816f270af44fdef1f8d77f70a24b74e
Raz Vaknin <razop90@gmail.com>
2019-08-09 19:18:54 +0300

    add recipe functions to server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const express = require('express')
 const app = express();
+ObjectId = require('mongodb').ObjectID;
 
 var mongo = require("mongoose");
 //instantiate a MongoDB database locally in lines 5–9.
@@ -37,13 +38,188 @@ var UserSchema = new Schema({
 name: { type: String },
 },{ versioinKey: false });
 
+var RecipeSchema = new Schema({
+    name: { type: String },
+    description: { type: String },
+    image_path: { type: String },
+    _user_id: { type: String },
+    ingredients: { type: Array },
+},{ versioinKey: false });
 
-var model = mongo.model('recipes', UserSchema, 'recipes');
-var modelUsers = mongo.model('uers', UserSchema, 'users');
+
+var modelRecipes = mongo.model('recipes', RecipeSchema, 'recipes');
+var modelUsers = mongo.model('users', UserSchema, 'users');
 var modelShoppingList = mongo.model('shoppingList', UserSchema, 'shoppingList');
 
 
-// In line 36–87 we have defined our API calls, which allow us to store, update, find and delete the user data from the database.
+// <------------------------------------Recipes------------------------------------------------------------------------------------->
+
+// Get all recipes and full user data of the creator of the recipe
+app.get("/api/getAllRecipes", function(req,res){
+		
+	modelRecipes.aggregate([
+		{ $lookup:
+			{
+				from: 'users',
+				localField: '_user_id',
+				foreignField: '_id',
+				as: 'user'
+			}
+		}], (err,results) => {
+			
+			 if(err){
+				console.log(err);
+				res.send("-1");
+			}
+			else {
+				console.log(results.length + " match recipes were found");
+				res.send(results);
+			}
+		})
+})
+
+// Get user's recipes by user id
+app.get("/api/getAllUserRecipes", function(req,res){
+	
+	var id = req.query.userId;
+	modelRecipes.find({_user_id: id}, function(err,data) {
+        if(err){
+			console.log(err);
+            res.send("-1");
+        }
+        else {
+			console.log(data.length + " match recipes were found for user id " + id);
+            res.send(data);
+        }
+    });
+})
+
+// Get recipe by id
+app.get("/api/getRecipe", function(req,res){
+	
+	var id = req.query.id;
+	modelRecipes.findOne({_id: id}, function(err,data) {
+        if(err || data == null){
+			console.log("A recipe with id " + id + " wasn't found");
+			
+			if(err != null)
+				console.log(err);
+			
+            res.send("-1");
+        }
+        else {
+			console.log("Got recipe " + id);
+            res.send(data);
+        }
+    });
+})
+
+// Remove recipe by id
+app.get("/api/deleteRecipe", function(req,res){
+
+	var id = req.query.id;
+    modelRecipes.deleteOne({_id: id}, function(err) {
+        if(err){
+            console.log(err)
+            res.send("-1");
+        }
+        else {
+			console.log("Recipe with id " + id + " has been removed")
+            res.send("0");
+        }
+    });
+})
+
+// Add new recipe
+app.get("/api/addNewRecipe", function(req,res){
+
+    console.log(req.query);
+
+    if(req.query.userId == null) {
+        console.log("No user id supplied to the insertion request! recipe not added")
+        res.send("-1")
+    }
+    else {
+        var user_id = req.query.userId;
+        var name = req.query.name ? req.query.name : "No name available";
+	    var description = req.query.description ? req.query.description : "No description available";
+        var image_path = req.query.image_path ? req.query.image_path : "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
+        var ingredients = req.query.ingredients.split(',');
+
+        var newRecipe = 
+        { 
+            "name": name, 
+            "description": description, 
+            "image_path": image_path, 
+            "_user_id": user_id,   
+            "ingredients": ingredients
+        };
+     
+        modelRecipes.create(newRecipe, function(err, data) {
+            if(err) {
+                console.log(err)
+                res.send("-1");
+            }
+            else {
+                console.log("Recipe with id " + data._id + " has been added")
+                console.log(data);
+                res.send("0");
+            }
+        });
+    } 
+})
+
+// update recipe
+app.get("/api/updateRecipe", function(req,res){
+
+    console.log(req.query);
+
+    var recipeId = req.query.id;
+    if(recipeId == null) {
+        console.log("No recipe id supplied to the updating request! recipe wasn't updated")
+        res.send("-1")
+    }
+    else {
+        // Update documents that match to the query
+        var searchQuery = { _id: recipeId };
+
+        // Updating the requested fields ONLY!
+        var newValues = {};
+        if (req.query.name) newValues.name = req.query.name;
+        if (req.query.description) newValues.description = req.query.description;
+        if (req.query.image_path) newValues.image_path = req.query.image_path;
+        if (req.query.ingredients) newValues.ingredients = req.query.ingredients.split(',');
+ 
+        modelRecipes.updateOne(searchQuery, newValues, function(err, updated_data) {
+            if(err) {
+                console.log(err)
+                res.send("-1");
+            }
+            else {
+                console.log("Recipe with id " + recipeId + " has been updated")
+                console.log(updated_data);
+                res.send("0");
+            }
+        });
+    } 
+})
+
+
+
+// <------------------------------------Users--------------------------------------------------------------------------------------->
+
+app.get("/api/getAllUsers", function(req,res){
+    modelUsers.find({}, function(err,data) {
+        if(err){
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+})
+
+// In line 89-140 we have defined our API calls, which allow us to store, update, find and delete the user data from the database.
 app.post("/api/saveUser", function(req,res){
     var mod = new model(req.body);
     if(req.body.mode == "Save")
@@ -86,8 +262,6 @@ app.post("/api/deleteUser", function(req,res){
     });
 })
 
-
-
 app.post("/api/getUser", function(req,res){
     model.find({}, function(err,data) {
         if(err){
@@ -99,27 +273,9 @@ app.post("/api/getUser", function(req,res){
     });
 })
 
-app.get("/api/getAllRecipes", function(req,res){
-    model.find({}, function(err,data) {
-        if(err){
-            res.send(err);
-        }
-        else {
-            res.send(data);
-        }
-    });
-})
 
-app.get("/api/getAllUsers", function(req,res){
-    modelUsers.find({}, function(err,data) {
-        if(err){
-            res.send(err);
-        }
-        else {
-            res.send(data);
-        }
-    });
-})
+
+// <------------------------------Shopping Lists------------------------------------------------------------------------------------>
 
 app.get("/api/getAllShoppingLists", function(req,res){
     modelShoppingList.find({}, function(err,data) {
@@ -146,6 +302,8 @@ app.get("/api/getShoppingListById", function(req,res){
         }
     });
 })
+
+
 
 app.listen(8080, function () {
     console.log('NodeJS server listening on port 8080...')
